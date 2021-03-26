1338a7666ba74e5dc3dc993db464cd28ec404015
Chris Song <chris@Chrisui-MacBookPro.local>
2021-03-26 11:46:36 +0900

    fix test

--- main_test.py
+++ main_test.py
@@ -6,7 +6,7 @@ import main
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
         ret = main.helloworld("Test")
-        self.assertEqual(ret, "Hello World! Chris!")
+        self.assertEqual(ret, "Hello World! Test!")
 
 
 if __name__ == "__main__":
