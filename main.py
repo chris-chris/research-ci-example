0e4e0a829e0928ca6b76a0607f5f75682a8ed8c1
Chris Song <chris@Chrisui-MacBookPro.local>
2021-03-26 12:00:06 +0900

    dup

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ def helloworld(a):
     print(msg)  # hmm
     return msg
 
+def helloworld(a):
+    msg = f"Hello World! {a}!"
+    print(msg)  # hmm
+    return msg
+
 
 if __name__ == "__main__":  # pragma: no cover
     helloworld("Chris")
